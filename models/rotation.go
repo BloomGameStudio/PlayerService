06de0bc01d2dd50772bfc08c34bf498e79a97ba6
Balu <baluproton+gitlab@protonmail.com>
2022-11-05 16:29:34 +0100

    Rotation Model

--- models/rotation.go
+++ models/rotation.go
@@ -2,6 +2,9 @@ package models
 
 type Rotation struct {
 	// Euler Rotation Annotation
-	// Yaw,Pitch,Roll TODO: Does this order reflect the client and is correct?
-	X, Y, Z float64
+	// Yaw,Pitch,Roll // TODO: Does this order reflect the client and is correct?
+	XYZ
+
+	// EulerAngles *XYZ `json:"eulerAngles"`
+	// Perhaps we want to have verbose EulerAngles that point to XYZ
 }
