5f49e6215a8a38361a82394602df4abfd1fda1aa
Balu <baluproton+gitlab@protonmail.com>
2022-11-05 16:27:52 +0100

    Position Model

---
+++ models/position.go
@@ -0,0 +1,8 @@
+package models
+
+type Position struct {
+	XYZ
+	// Normalized   *Position `json:"normalized,omitempty"`
+	// Magnitude    float64   `json:"magnitude"`
+	// SqrMagnitude float64   `json:"sqrMagnitude"`
+}
