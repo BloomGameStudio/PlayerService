87fc4fa412ebe16af2b0bd74ae2fed2475fa080e
Balu <baluproton+gitlab@protonmail.com>
2022-11-05 16:33:36 +0100

    Player Model

--- models/player.go
+++ models/player.go
@@ -5,8 +5,13 @@ import (
 )
 
 type Player struct {
-	UserID   uuid.UUID `gorm:"type:uuid;primarykey"`
-	Location Coordinate
-	Rotation Rotation
-	State    State
+	UserID uuid.UUID `gorm:"type:uuid;primarykey"`
+	// TODO: @Lapras What exactly does Name represent? Is it not always "GameObject"? and therefore could be infered? Or completly emited?
+	Name string `json:"name"`
+	// TODO: @Lapras Do we need a layer? Is the player not always on the PlayableCharacters layer or at least on the same consistante layer
+	Layer    string   `json:"layer"`
+	Position Position `json:"position"`
+	Rotation Rotation `json:"rotation"`
+	Scale    XYZ      `json:"scale"`
+	// State    State
 }
