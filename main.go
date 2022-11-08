3a2f124d544ca30f265050346a35544bea05f442
Balu <baluproton+gitlab@protonmail.com>
2022-11-08 01:02:44 +0100

    Ping Route

--- main.go
+++ main.go
@@ -13,10 +13,14 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// Testing routes
+	e.GET("ping", controllers.Ping)
 	e.File("/", "public/index.html")
-
 	// ws://localhost:1323/ws
 	e.GET("/ws", controllers.Hello)
+	// End of testing routes
+
+	e.POST("player", controllers.CreatePlayer)
 
 	e.Logger.Fatal(e.Start(":1323"))
 
