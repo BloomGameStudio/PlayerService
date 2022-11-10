074c095c629cd10a04e9ef442dacda80995bfee6
lapras <lapras@laprass-MacBook-Pro.local>
2022-11-09 18:51:46 -1000

    Removed `layer` tag question - it is needed and will differ between players

--- models/player.go
+++ models/player.go
@@ -12,7 +12,6 @@ type Player struct {
 	UserID uuid.UUID `gorm:"type:uuid;index"`
 	// TODO: @Lapras What exactly does Name represent? Is it not always "GameObject"? and therefore could be infered? Or completly emited?
 	Name string `json:"name"`
-	// TODO: @Lapras Do we need a layer? Is the player not always on the PlayableCharacters layer or at least on the same consistante layer
 	Layer      string `json:"layer"`
 	PositionID int
 	Position   Position `json:"position" `
