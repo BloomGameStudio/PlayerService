f42eecf899e76ab38d7d1cea8cf9e6bf2f3b56c0
lapras <lapras@bloomcollective.gg>
2022-11-09 19:01:04 -1000

    Removing `name` tag question - we do want name as it will be a unique identifier in time

--- models/player.go
+++ models/player.go
@@ -10,8 +10,7 @@ import (
 type Player struct {
 	gorm.Model
 	UserID uuid.UUID `gorm:"type:uuid;index"`
-	// TODO: @Lapras What exactly does Name represent? Is it not always "GameObject"? and therefore could be infered? Or completly emited?
-	Name string `json:"name"`
+	Name 		string `json:"name"`
 	Layer      string `json:"layer"`
 	PositionID int
 	Position   Position `json:"position" `
