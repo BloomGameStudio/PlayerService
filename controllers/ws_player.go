d67a132f0d9f75ea40b1253af522091247f7529a
Balu <baluproton+gitlab@protonmail.com>
2022-11-15 22:45:20 +0100

    Websocket Player Write & Docstring

--- controllers/ws_player.go
+++ controllers/ws_player.go
@@ -1,24 +1,46 @@
 package controllers
 
 import (
+	"github.com/BloomGameStudio/PlayerService/database"
 	"github.com/BloomGameStudio/PlayerService/handlers"
-	"github.com/gorilla/websocket"
+	"github.com/BloomGameStudio/PlayerService/models"
 	"github.com/labstack/echo/v4"
 )
 
 func Player(c echo.Context) error {
+
+	// QUESTION: Is this needed?
+	// Only changes will be sent the only exception to this is the opening/first request where the full state will be sent
+	// Partial player data can be received or full
+	// TODO: Partial Reads
+	// TODO: Partial Writes
+
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
 		return err
 	}
 	defer ws.Close()
 
+	// Open DB outside of loopception
+	db := database.Open()
+
 	for {
+
 		// Write
-		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-		if err != nil {
-			c.Logger().Error(err)
-		}
+		func() {
+
+			// Get all the players
+			players := &models.Player{}
+			db.Find(players)
+
+			// Find/Filter the Changes that occured in the players and send them
+			// PlayerChanges(players,players)
+
+			err := ws.WriteJSON(players)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}()
 
 		// Read
 		func() {
