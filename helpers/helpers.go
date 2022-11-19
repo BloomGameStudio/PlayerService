bfe560a99bc218f983f578eb7be8722fda2fe5d3
Balu <baluproton+gitlab@protonmail.com>
2022-11-19 22:48:54 +0100

    Pretty String Helper

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"bytes"
 	"encoding/json"
 
 	"github.com/BloomGameStudio/PlayerService/database"
@@ -48,4 +49,12 @@ func PrettyStructNoError(data interface{}) string {
 		return ""
 	}
 	return string(val)
+}
+
+func PrettyString(str string) (string, error) {
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, []byte(str), "", "    "); err != nil {
+		return "", err
+	}
+	return prettyJSON.String(), nil
 }
