c75ab33cab7af02e121fae4c78e93c99163f7d6e
Balu <baluproton+gitlab@protonmail.com>
2022-11-20 16:06:16 +0100

    Async WebSocket Player Reader & Writer

--- controllers/ws_player.go
+++ controllers/ws_player.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"errors"
+	"time"
+
 	"github.com/BloomGameStudio/PlayerService/database"
 	"github.com/BloomGameStudio/PlayerService/models"
 	"github.com/BloomGameStudio/PlayerService/publicModels"
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 	"gorm.io/gorm/clause"
@@ -23,9 +27,6 @@ func Player(c echo.Context) error {
 	}
 	defer ws.Close()
 
-	// Open DB outside of loopception
-	db := database.Open()
-
 	// OPTIMIZE: Use GetUserIDFromJWT function to avoid db call
 	// TODO: UNCOMNNET
 	// player, err := helpers.GetPlayerModelFromJWT(c)
@@ -35,86 +36,121 @@ func Player(c echo.Context) error {
 	// }
 	// playerUserID := player.UserID
 
+	// TODO: Close connection handling
+
+	writerChan := make(chan error)
+	readerChan := make(chan error)
+
+	go writer(c, ws, writerChan)
+	go reader(c, ws, readerChan)
+
+	// QUESTION: Do we want to wait on both routines to error out?
+	// Return the error if either the reader or the writer encounters a error
 	for {
-
-		// TODO: Close connection handling
-
-		// Write
-		func() {
-
-			c.Logger().Debug("Writing to the WebSocket")
-
-			c.Logger().Debug("Getting all players from the database")
-			// Get all the players
-			players := &models.Player{}
-			db.Preload(clause.Associations).Find(players)
-
-			// Find/Filter the Changes that occured in the players and send them
-			// PlayerChanges(players,players)
-
-			c.Logger().Debug("Pushing the player to the WebSocket")
-			err := ws.WriteJSON(players)
-			if err != nil {
-				c.Logger().Error(err)
-			}
-			c.Logger().Debug("Finished writing to the WebSocket")
-
-			// time.Sleep(time.Second * 3)
-
-		}()
-
-		// Read
-		func() {
-
-			// TODO: THIS IS VULNARABLE CLIENTS CAN CHANGE OBJECT IDS especially the nested ones!!!
-			// TODO: NO VALIDATION OF INPUT DATA IS PERFORMED!!!
-
-			c.Logger().Debug("Reading from the WebSocket")
-
-			// Initializer request player to bind into
-			reqPlayer := &publicModels.Player{}
-			err := ws.ReadJSON(reqPlayer)
-
-			if err != nil {
-				c.Logger().Debug("We get an error from Reading the JSON reqPlayer")
-				c.Logger().Error(err)
-			}
-
-			c.Logger().Debugf("reqPlayer from the WebSocket: %+v", reqPlayer)
-
-			c.Logger().Debug("Validating reqPlayer")
-			if !reqPlayer.IsValid() {
-				c.Logger().Debug("reqPlayer is NOT valid returning")
-				return
-			}
-
-			c.Logger().Debug("reqPlayer is valid")
-
-			c.Logger().Debug("Initializing and populating player model!")
-			// Use dot annotation for promoted aka embedded fields.
-			playerModel := &models.Player{}
-			// TODO: Handle UserID and production mode
-			playerModel.Position = reqPlayer.Position
-			playerModel.Rotation = reqPlayer.Rotation
-			playerModel.Scale = reqPlayer.Scale
-
-			if viper.GetBool("DEBUG") {
-				// Add the Player.Name in DEBUG mode that it can be used as ID in the Player handle to avoid the Userservice dependency
-				playerModel.Name = reqPlayer.Name
-			}
-
-			c.Logger().Debugf("playerModel: %+v", playerModel)
-
-			c.Logger().Debug("Validating playerModel")
-			if !playerModel.IsValid() {
-				c.Logger().Debug("playerModel is NOT valid returning")
-				return
-			}
-
-			c.Logger().Debug("playerModel is valid passing it to the Player handler")
-			// handlers.Player(*playerModel) //TODO: UNCOMNNET
-
-		}()
-
+		select {
+		case r := <-readerChan:
+			c.Logger().Debugf("Recieved readerChan error: %v", r)
+			return r
+		case w := <-writerChan:
+			c.Logger().Debugf("Recieved writerChan error: %v", w)
+			return w
+		}
 	}
+}
+
+// Write
+func writer(c echo.Context, ws *websocket.Conn, ch chan error) {
+
+	// Open DB outside of the loop
+	db := database.Open()
+
+	for {
+		// TODO: The Entire Player Model is being sent. It may contain information that should not be sent!
+		c.Logger().Debug("Writing to the WebSocket")
+
+		c.Logger().Debug("Getting all players from the database")
+		// Get all the players
+		players := &models.Player{}
+		db.Preload(clause.Associations).Find(players)
+
+		// Find/Filter the Changes that occured in the players and send them
+		// PlayerChanges(players,players)
+
+		c.Logger().Debug("Pushing the player to the WebSocket")
+		err := ws.WriteJSON(players)
+		if err != nil {
+			c.Logger().Error(err)
+			ch <- err
+			break
+
+		}
+		c.Logger().Debug("Finished writing to the WebSocket Sleeping now")
+
+		// Update Interval NOTE: setting depending on the server and its performance either increase or decrease it.
+		time.Sleep(time.Millisecond * 1)
+
+		if viper.GetBool("DEBUG") {
+			// Sleep for 1 second in DEBUG mode to not get fludded with data
+			time.Sleep(time.Second * 1)
+		}
+	}
+}
+
+// Read
+func reader(c echo.Context, ws *websocket.Conn, ch chan error) {
+
+	// TODO: THIS IS VULNARABLE CLIENTS CAN CHANGE OBJECT IDS especially the nested ones!!!
+	// TODO: NO VALIDATION OF INPUT DATA IS PERFORMED!!!
+
+	for {
+		c.Logger().Debug("Reading from the WebSocket")
+
+		// Initializer request player to bind into
+		reqPlayer := &publicModels.Player{}
+		err := ws.ReadJSON(reqPlayer)
+
+		if err != nil {
+			c.Logger().Debug("We get an error from Reading the JSON reqPlayer")
+			c.Logger().Error(err)
+			ch <- err
+			break
+		}
+
+		c.Logger().Debugf("reqPlayer from the WebSocket: %+v", reqPlayer)
+
+		c.Logger().Debug("Validating reqPlayer")
+		if !reqPlayer.IsValid() {
+			c.Logger().Debug("reqPlayer is NOT valid returning")
+			ch <- errors.New("reqPlayer Validation failed")
+			break
+		}
+
+		c.Logger().Debug("reqPlayer is valid")
+
+		c.Logger().Debug("Initializing and populating player model!")
+		// Use dot annotation for promoted aka embedded fields.
+		playerModel := &models.Player{}
+		// TODO: Handle UserID and production mode
+		playerModel.Position = reqPlayer.Position
+		playerModel.Rotation = reqPlayer.Rotation
+		playerModel.Scale = reqPlayer.Scale
+
+		if viper.GetBool("DEBUG") {
+			// Add the Player.Name in DEBUG mode that it can be used as ID in the Player handle to avoid the Userservice dependency
+			playerModel.Name = reqPlayer.Name
+		}
+
+		c.Logger().Debugf("playerModel: %+v", playerModel)
+
+		c.Logger().Debug("Validating playerModel")
+		if !playerModel.IsValid() {
+			c.Logger().Debug("playerModel is NOT valid returning")
+			ch <- errors.New("playerModel Validation failed")
+			break
+		}
+
+		c.Logger().Debug("playerModel is valid passing it to the Player handler")
+		// handlers.Player(*playerModel) //TODO: UNCOMNNET
+	}
+
 }
