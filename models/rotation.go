3816928da62fa57ef0ca6dbdc90e206fae079142
lapras <lapras@bloomcollective.gg>
2023-02-06 08:16:11 -1000

    Adding w var

--- models/rotation.go
+++ models/rotation.go
@@ -7,6 +7,7 @@ type Rotation struct {
 	// Euler Rotation Annotation
 	// Yaw,Pitch,Roll
 	Vector3
+	w float32
 
 	// EulerAngles *Vector3 `json:"eulerAngles"`
 	// Perhaps we want to have verbose EulerAngles that point to a Vector3
