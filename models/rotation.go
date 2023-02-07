1899e5672a9308929701e42066d9ad4072a64d73
Balu <baluproton+gitlab@protonmail.com>
2023-02-07 14:18:57 +0100

    Rotaion Update "W"

--- models/rotation.go
+++ models/rotation.go
@@ -7,6 +7,7 @@ type Rotation struct {
 	// Euler Rotation Annotation
 	// Yaw,Pitch,Roll
 	Vector3
+	W float64 `json:"w"`
 
 	// EulerAngles *Vector3 `json:"eulerAngles"`
 	// Perhaps we want to have verbose EulerAngles that point to a Vector3
