b685b6a109d4014652eb2d222cb8b806a48c7b5f
Balu <baluproton+gitlab@protonmail.com>
2023-07-15 22:38:39 +0200

    Add UserId to health Model

--- models/health.go
+++ models/health.go
@@ -2,11 +2,13 @@ package models
 
 import (
 	"github.com/BloomGameStudio/PlayerService/publicModels"
+	uuid "github.com/satori/go.uuid"
 	"gorm.io/gorm"
 )
 
 type Health struct {
 	gorm.Model
+	UserID uuid.UUID `gorm:"type:uuid;uniqueIndex"`
 	publicModels.Attribute
 }
 
