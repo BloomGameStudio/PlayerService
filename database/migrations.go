0d8bf5dba4fa298bfc5a0123d1d56d19f231e08e
Balu <baluproton+gitlab@protonmail.com>
2023-07-15 21:02:24 +0200

    Health Migration

--- database/migrations.go
+++ database/migrations.go
@@ -1,5 +1,10 @@
 package database
 
+import "github.com/BloomGameStudio/PlayerService/models"
+
 func Migrate() {
 
+	db := Open()
+	// This will Auto Migrate all its nested structs
+	db.AutoMigrate(&models.Health{})
 }
