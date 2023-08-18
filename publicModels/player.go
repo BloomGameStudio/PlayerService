29ae8406a9b16970e505eb21b36dd123e62d255b
Balu <baluproton+gitlab@protonmail.com>
2023-08-18 20:41:49 +0200

    Move Rotation Scale and Poisition into Transform

--- publicModels/player.go
+++ publicModels/player.go
@@ -4,16 +4,10 @@ type Player struct {
 	// publicModels.Player holds public fields for the player model.
 
 	// COMBAK: Add needed further fields from the Player struct model
-	Name       string `json:"name" gorm:"uniqueIndex"`
-	Layer      string `json:"layer"`
-	PositionID uint
-	Position   Position `json:"position" `
-	RotationID uint
-	Rotation   Rotation `json:"rotation" `
-	ScaleID    uint
-	Scale      Scale  `json:"scale"`
-	ENS        string `json:"ens"`
-	Active     bool   `json:"active" gorm:"default:1"` // default to true only tested on SQLite might behave differently on other databases
+	Name   string `json:"name" gorm:"uniqueIndex"`
+	Layer  string `json:"layer"`
+	ENS    string `json:"ens"`
+	Active bool   `json:"active" gorm:"default:1"` // default to true only tested on SQLite might behave differently on other databases
 }
 
 func (p Player) IsValid() bool {
@@ -21,18 +15,6 @@ func (p Player) IsValid() bool {
 	// Validates the public Player
 	// Additional validation and hooks for the public Player validation can be added here
 	// WARNING: Validation should be scoped to the public Player
-
-	if !p.Position.IsValid() {
-		return false
-	}
-
-	if !p.Rotation.IsValid() {
-		return false
-	}
-
-	if !p.Scale.IsValid() {
-		return false
-	}
 
 	return true
 }
--- publicModels/transform.go
+++ publicModels/transform.go
@@ -4,9 +4,27 @@ import "gorm.io/gorm"
 
 type Transform struct {
 	gorm.Model
+	PositionID uint
+	Position   Position `json:"position" `
+	RotationID uint
+	Rotation   Rotation `json:"rotation" `
+	ScaleID    uint
+	Scale      Scale `json:"scale"`
 }
 
 func (s Transform) IsValid() bool {
+
+	if !s.Position.IsValid() {
+		return false
+	}
+
+	if !s.Rotation.IsValid() {
+		return false
+	}
+
+	if !s.Scale.IsValid() {
+		return false
+	}
 
 	return true
 }
