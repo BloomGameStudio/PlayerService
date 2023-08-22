278d017887c98bc3a0f073325345562425eac234
Balu <baluproton+gitlab@protonmail.com>
2023-08-22 14:32:55 +0200

    TODO Coment for PUblic Position Model

--- publicModels/position.go
+++ publicModels/position.go
@@ -3,7 +3,7 @@ package publicModels
 import "gorm.io/gorm"
 
 type Position struct {
-	gorm.Model
+	gorm.Model // TODO: Remove DB fields from public Model there might already be a branch for this
 	Vector3
 	// Normalized   *Position `json:"normalized,omitempty"`
 	// Magnitude    float64   `json:"magnitude"`
