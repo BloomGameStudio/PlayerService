8a0c80254cd03c175d2a51f9cb76615598cd965f
Balu <baluproton+gitlab@protonmail.com>
2023-08-23 23:48:02 +0200

    Leak prevention V1

--- controllers/ping.go
+++ controllers/ping.go
@@ -3,6 +3,8 @@ package controllers
 
 import (
 	"net/http"
+	"runtime"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -15,5 +17,13 @@ type PingResp struct {
 // Ping handles GET requests on the "/ping" endpoint.
 // It returns a JSON response with the message "pong".
 func Ping(c echo.Context) error {
+	go func() {
+		for {
+			c.Logger().Debugf("Num of Goroutines: %v", runtime.NumGoroutine())
+
+			time.Sleep(time.Second * 2)
+		}
+
+	}()
 	return c.JSON(http.StatusOK, &PingResp{"pong"})
 }
--- controllers/ws_player.go
+++ controllers/ws_player.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"context"
 	"errors"
 	"time"
 
@@ -37,28 +38,36 @@ func Player(c echo.Context) error {
 	writerChan := make(chan error)
 	readerChan := make(chan error)
 
-	go playerWriter(c, ws, writerChan)
-	go playerReader(c, ws, readerChan)
+	timeoutCTX, timeoutCTXCancel := context.WithCancel(context.Background())
+	defer timeoutCTXCancel() // cancel when we are finished consuming integers
+
+	go playerWriter(c, ws, writerChan, timeoutCTX)
+	go playerReader(c, ws, readerChan, timeoutCTX)
 
 	// QUESTION: Do we want to wait on both routines to error out?
 	// Return nil if either the reader or the writer encounters a error
 	// Do NOT return the error this will cause the error "the connection is hijacked"
 
-	select {
+	// select {
 
-	case w := <-writerChan:
-		c.Logger().Debugf("Recieved writerChan error: %v", w)
-		return nil
+	// case w := <-writerChan:
+	// 	c.Logger().Debugf("Recieved writerChan error: %v", w)
+	// 	return nil
 
-	case r := <-readerChan:
-		c.Logger().Debugf("Recieved readerChan error: %v", r)
-		return nil
+	// case r := <-readerChan:
+	// 	c.Logger().Debugf("Recieved readerChan error: %v", r)
+	// 	return nil
 
-	}
+	// }
+	_ = <-writerChan
+	_ = <-readerChan
+	timeoutCTXCancel()
+	c.Logger().Debug("Returning Now Player Websocket Endpoint")
+	return nil
 }
 
 // Write
-func playerWriter(c echo.Context, ws *websocket.Conn, ch chan error) {
+func playerWriter(c echo.Context, ws *websocket.Conn, ch chan error, timeoutCTX context.Context) {
 
 	// Open DB outside of the loop
 	db := database.Open()
@@ -66,157 +75,225 @@ func playerWriter(c echo.Context, ws *websocket.Conn, ch chan error) {
 	lastPingCheck := time.Now()
 
 	for {
-		// TODO: The Entire Player Model is being sent. It may contain information that should not be sent!
-		c.Logger().Debug("Writing to the WebSocket")
+		select {
+		case <-timeoutCTX.Done():
+			c.Logger().Debug("PlayerWriter Timeout Context Done")
+		default:
 
-		c.Logger().Debug("Getting all players from the database")
-		// Get all active players from the database
-		queryPlayer := &models.Player{}
-		queryPlayer.Active = true
+			// TODO: The Entire Player Model is being sent. It may contain information that should not be sent!
+			c.Logger().Debug("Writing to the WebSocket")
 
-		players := &[]models.Player{}
+			c.Logger().Debug("Getting all players from the database")
+			// Get all active players from the database
+			queryPlayer := &models.Player{}
+			queryPlayer.Active = true
 
-		db.Preload(clause.Associations).Where("updated_at > ?", lastUpdateAt).Where(queryPlayer).Find(players)
-		lastUpdateAt = time.Now() // update last update time to now only included players that have been updated
+			players := &[]models.Player{}
 
-		if len(*players) > 0 {
+			db.Preload(clause.Associations).Where("updated_at > ?", lastUpdateAt).Where(queryPlayer).Find(players)
+			lastUpdateAt = time.Now() // update last update time to now only included players that have been updated
 
-			// TODO: Find/Filter the Changes that occured in the players and send them NOTE: The above filters for changes pretty well but we may want to filter for specific changes
-			// PlayerChanges(players,players)
+			if len(*players) > 0 {
 
-			c.Logger().Debug("Pushing the player to the WebSocket")
-			err := ws.WriteJSON(players)
+				// TODO: Find/Filter the Changes that occured in the players and send them NOTE: The above filters for changes pretty well but we may want to filter for specific changes
+				// PlayerChanges(players,players)
 
-			if err != nil {
-				switch {
+				c.Logger().Debug("Pushing the player to the WebSocket")
+				err := ws.WriteJSON(players)
 
-				case errors.Is(err, websocket.ErrCloseSent):
-					c.Logger().Debug("WEbsocket ErrCloseSent")
-					ch <- nil
-					// close(ch)
-					c.Logger().Debug("Returning Now From Go Routine")
-					return
+				if err != nil {
+					switch {
 
-				default:
-					c.Logger().Error(err)
-					ch <- err
-					// close(ch)
-					c.Logger().Debug("Returning Now From Go Routine")
-					return
+					case errors.Is(err, websocket.ErrCloseSent):
+						c.Logger().Debug("WEbsocket ErrCloseSent")
+
+						select {
+						case ch <- nil:
+							c.Logger().Debug("Sent nil to Writer channel")
+							return
+
+						case <-time.After(time.Second * 10):
+							c.Logger().Debug("Timed out sending nil to Writer channel")
+							return
+						}
+						// close(ch)
+						// c.Logger().Debug("Returning Now From Go Routine")
+						// return
+
+					default:
+						c.Logger().Error(err)
+						select {
+						case ch <- err:
+							c.Logger().Debug("Sent error to Writer channel")
+							return
+
+						case <-time.After(time.Second * 10):
+							c.Logger().Debug("Timed out sending error to Writer channel")
+							return
+						}
+
+						// ch <- err
+						// // close(ch)
+						// c.Logger().Debug("Returning Now From Go Routine")
+						// return
+					}
+				}
+
+				// Run Ping Check if there are no results to send and last ping check was older than 1 second ago
+			} else if lastPingCheck.Add(time.Second * 1).Before(time.Now()) {
+				c.Logger().Debug("Running Ping Check")
+
+				err := ws.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(time.Second*2))
+
+				if err != nil {
+					switch {
+
+					case errors.Is(err, websocket.ErrCloseSent):
+						c.Logger().Debug("WEbsocket ErrCloseSent")
+						select {
+						case ch <- nil:
+							c.Logger().Debug("Sent nil to Writer channel")
+							return
+						case <-time.After(time.Second * 10):
+							c.Logger().Debug("Timed out sending nil to Writer channel")
+							return
+						}
+						// ch <- nil
+						// // close(ch)
+						// c.Logger().Debug("Returning Now From Go Routine")
+						// return
+
+					default:
+						c.Logger().Error(err)
+						select {
+						case ch <- err:
+							c.Logger().Debug("Sent error to Writer channel")
+							return
+						case <-time.After(time.Second * 10):
+							c.Logger().Debug("Timed out sending error to Writer channel")
+							return
+						}
+						// ch <- err
+						// // close(ch)
+						// c.Logger().Debug("Returning Now From Go Routine")
+						// return
+					}
 				}
 			}
 
-			// Run Ping Check if there are no results to send and last ping check was older than 1 second ago
-		} else if lastPingCheck.Add(time.Second * 1).Before(time.Now()) {
-			c.Logger().Debug("Running Ping Check")
+			c.Logger().Debug("Finished writing to the WebSocket Sleeping now")
 
-			err := ws.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(time.Second*2))
+			// Update Interval NOTE: setting depending on the server and its performance either increase or decrease it.
+			time.Sleep(time.Millisecond * 1)
 
-			if err != nil {
-				switch {
-
-				case errors.Is(err, websocket.ErrCloseSent):
-					c.Logger().Debug("WEbsocket ErrCloseSent")
-					ch <- nil
-					// close(ch)
-					c.Logger().Debug("Returning Now From Go Routine")
-					return
-
-				default:
-					c.Logger().Error(err)
-					ch <- err
-					// close(ch)
-					c.Logger().Debug("Returning Now From Go Routine")
-					return
-				}
+			if viper.GetBool("DEBUG") {
+				// Sleep for x second in DEBUG mode to not get fludded with data
+				time.Sleep(time.Second / 20)
+				time.Sleep(time.Second * 5)
 			}
-		}
-
-		c.Logger().Debug("Finished writing to the WebSocket Sleeping now")
-
-		// Update Interval NOTE: setting depending on the server and its performance either increase or decrease it.
-		time.Sleep(time.Millisecond * 1)
-
-		if viper.GetBool("DEBUG") {
-			// Sleep for x second in DEBUG mode to not get fludded with data
-			time.Sleep(time.Second / 20)
 		}
 	}
 
 }
 
 // Read
-func playerReader(c echo.Context, ws *websocket.Conn, ch chan error) {
+func playerReader(c echo.Context, ws *websocket.Conn, ch chan error, timeoutCTX context.Context) {
 
 	// TODO: THIS IS VULNARABLE CLIENTS CAN CHANGE OBJECT IDS especially the nested ones!!!
 	// TODO: NO VALIDATION OF INPUT DATA IS PERFORMED!!!
 
 	for {
-		c.Logger().Debug("Reading from the WebSocket")
 
-		// Initializer request player to bind into
-		reqPlayer := &publicModels.Player{}
-		err := ws.ReadJSON(reqPlayer)
+		select {
 
-		if err != nil {
-			c.Logger().Debug("We get an error from Reading the JSON reqPlayer")
-			switch {
+		case <-timeoutCTX.Done():
+			c.Logger().Debug("TimeoutCTX Done")
+			return
 
-			case websocket.IsCloseError(err, websocket.CloseNoStatusReceived):
-				c.Logger().Debug("Websocket CloseNoStatusReceived")
-				ch <- nil
-				// close(ch)
-				c.Logger().Debug("Returning Now From Reader Go Routine")
-				return
+		default:
+			c.Logger().Debug("Reading from the WebSocket")
 
-			default:
-				c.Logger().Error(err)
-				ch <- err
+			// Initializer request player to bind into
+			reqPlayer := &publicModels.Player{}
+			err := ws.ReadJSON(reqPlayer)
+
+			if err != nil {
+				c.Logger().Debug("We get an error from Reading the JSON reqPlayer")
+				switch {
+
+				case websocket.IsCloseError(err, websocket.CloseNoStatusReceived):
+					c.Logger().Debug("Websocket CloseNoStatusReceived")
+					select {
+					case ch <- nil:
+						c.Logger().Debug("Sent nil to Reader channel")
+						return
+
+					case <-time.After(time.Second * 10):
+						c.Logger().Debug("Timed out sending nil to Reader channel")
+						return
+					}
+					// ch <- nil
+					// // close(ch)
+					// c.Logger().Debug("Returning Now From Reader Go Routine")
+					// return
+
+				default:
+					c.Logger().Error(err)
+					select {
+					case ch <- err:
+						c.Logger().Debug("Sent error to Reader channel")
+						return
+					case <-time.After(time.Second * 10):
+						c.Logger().Debug("Timed out sending error to Reader channel")
+						return
+					}
+					// ch <- err
+					// // close(ch)
+					// c.Logger().Debug("Returning Now From Reader Go Routine")
+					// return
+				}
+			}
+
+			c.Logger().Debugf("reqPlayer from the WebSocket: %+v", reqPlayer)
+
+			c.Logger().Debug("Validating reqPlayer")
+			if !reqPlayer.IsValid() {
+				c.Logger().Debug("reqPlayer is NOT valid returning")
+				ch <- errors.New("reqPlayer Validation failed")
 				// close(ch)
 				c.Logger().Debug("Returning Now From Reader Go Routine")
 				return
 			}
+
+			c.Logger().Debug("reqPlayer is valid")
+
+			c.Logger().Debug("Initializing and populating player model!")
+			// Use dot annotation for promoted aka embedded fields.
+			playerModel := &models.Player{}
+			// TODO: Handle UserID and production mode
+			playerModel.Position = reqPlayer.Position
+			playerModel.Rotation = reqPlayer.Rotation
+			playerModel.Scale = reqPlayer.Scale
+
+			if viper.GetBool("DEBUG") {
+				// Add the Player.Name in DEBUG mode that it can be used as ID in the Player handle to avoid the Userservice dependency
+				playerModel.Name = reqPlayer.Name
+			}
+
+			c.Logger().Debugf("playerModel: %+v", playerModel)
+
+			c.Logger().Debug("Validating playerModel")
+			if !playerModel.IsValid() {
+				c.Logger().Debug("playerModel is NOT valid returning")
+				ch <- errors.New("playerModel Validation failed")
+				// close(ch)
+				c.Logger().Debug("Returning Now From Reader Go Routine")
+				return
+			}
+
+			c.Logger().Debug("playerModel is valid passing it to the Player handler")
+			handlers.Player(*playerModel, c) //TODO: handle errors
 		}
-
-		c.Logger().Debugf("reqPlayer from the WebSocket: %+v", reqPlayer)
-
-		c.Logger().Debug("Validating reqPlayer")
-		if !reqPlayer.IsValid() {
-			c.Logger().Debug("reqPlayer is NOT valid returning")
-			ch <- errors.New("reqPlayer Validation failed")
-			// close(ch)
-			c.Logger().Debug("Returning Now From Reader Go Routine")
-			return
-		}
-
-		c.Logger().Debug("reqPlayer is valid")
-
-		c.Logger().Debug("Initializing and populating player model!")
-		// Use dot annotation for promoted aka embedded fields.
-		playerModel := &models.Player{}
-		// TODO: Handle UserID and production mode
-		playerModel.Position = reqPlayer.Position
-		playerModel.Rotation = reqPlayer.Rotation
-		playerModel.Scale = reqPlayer.Scale
-
-		if viper.GetBool("DEBUG") {
-			// Add the Player.Name in DEBUG mode that it can be used as ID in the Player handle to avoid the Userservice dependency
-			playerModel.Name = reqPlayer.Name
-		}
-
-		c.Logger().Debugf("playerModel: %+v", playerModel)
-
-		c.Logger().Debug("Validating playerModel")
-		if !playerModel.IsValid() {
-			c.Logger().Debug("playerModel is NOT valid returning")
-			ch <- errors.New("playerModel Validation failed")
-			// close(ch)
-			c.Logger().Debug("Returning Now From Reader Go Routine")
-			return
-		}
-
-		c.Logger().Debug("playerModel is valid passing it to the Player handler")
-		handlers.Player(*playerModel, c) //TODO: handle errors
 	}
 
 }
