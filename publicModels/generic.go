bb2d23c12b3120821ea6066b6ad624f66c0ee0ef
Balu <baluproton+gitlab@protonmail.com>
2023-08-26 11:37:43 +0200

    Updated Public Models Shema

--- publicModels/generic.go
+++ publicModels/generic.go
@@ -1,6 +1,7 @@
 package publicModels
 
 type Vector3 struct {
+	// gorm.Model         // HACK: TODO: Remove this and derive fields This is for debug mode only people can provide IDs
 	X float64 `json:"x"`
 	Y float64 `json:"y"`
 	Z float64 `json:"z"`
