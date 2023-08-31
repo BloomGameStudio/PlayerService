b5f27567ac3a20d429a33dda281e29eaf6fede9d
Balu <baluproton+gitlab@protonmail.com>
2023-08-31 23:10:53 +0200

    Private State

--- models/state.go
+++ models/state.go
@@ -1,14 +1,29 @@
 package models
 
+import (
+	"github.com/BloomGameStudio/PlayerService/publicModels"
+	"gorm.io/gorm"
+)
+
 type State struct {
-	Grounded, Airborn, Waterborn bool
+	gorm.Model
+	PlayerID uint
+	publicModels.State
+
+	// Grounded, Airborn, Waterborn bool
 }
 
-func (p State) IsValid() bool {
+// type States []State
 
-	// Validates the State
-	// Additional validation and hooks for the State validation can be added here
-	// WARNING: Validation should be scoped to the State
+// type States struct {
+// 	States []State `json:"states"`
+// }
 
-	return true
-}
+// func (p State) IsValid() bool {
+
+// 	// Validates the State
+// 	// Additional validation and hooks for the State validation can be added here
+// 	// WARNING: Validation should be scoped to the State
+
+// 	return true
+// }
