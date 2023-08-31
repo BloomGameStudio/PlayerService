28e39770ec22e6595fb223ff3981ec82028ce27d
Balu <baluproton+gitlab@protonmail.com>
2023-08-31 23:09:34 +0200

    PUblic State

--- publicModels/state.go
+++ publicModels/state.go
@@ -2,15 +2,22 @@ package publicModels
 
 type State struct {
 	// publicModels.State holds public fields for the State model.
-
-	Grounded, Airborn, Waterborn bool
+	StateID uint    `json:"stateID"`
+	Value   float64 `json:"value"`
+	// Grounded, Airborn, Waterborn bool
 }
 
-func (p State) IsValid() bool {
+// type States []State
 
-	// Validates the public State
-	// Additional validation and hooks for the public State validation can be added here
-	// WARNING: Validation should be scoped to the public State
+// type States struct {
+// 	States []State `json:"states"`
+// }
 
-	return true
-}
+// func (p State) IsValid() bool {
+
+// 	// Validates the public State
+// 	// Additional validation and hooks for the public State validation can be added here
+// 	// WARNING: Validation should be scoped to the public State
+
+// 	return true
+// }
