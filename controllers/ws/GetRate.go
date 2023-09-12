73e9da70315950a3a155d6fa57c8250c25207da8
ghjor <joriander@protonmail.com>
2023-09-12 06:47:41 -0700

    changed GetRate func to standard Go func declaration

--- controllers/ws/GetRate.go
+++ controllers/ws/GetRate.go
@@ -6,14 +6,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var (
-	GetRate = func(c echo.Context) int {
-		params := c.Request().URL.Query()
-		rateStr := params.Get("rate")
-		rate, err := strconv.Atoi(rateStr)
-		if err != nil {
-			rate = 1
-		}
-		return rate
+func GetRate(c echo.Context) int {
+	params := c.Request().URL.Query()
+	rateStr := params.Get("rate")
+	rate, err := strconv.Atoi(rateStr)
+	if err != nil {
+		rate = 1
 	}
-)
+	return rate
+}
