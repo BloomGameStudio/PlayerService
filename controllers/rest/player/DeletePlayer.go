ed2c0246f563d0dbda24d93394e93f83e0b791d9
Sarahtonein <joemoe1899@gmail.com>
2023-09-14 15:24:22 +1000

    Remove DB search as unneeded

--- controllers/rest/player/DeletePlayer.go
+++ controllers/rest/player/DeletePlayer.go
@@ -5,7 +5,6 @@ import (
 	"github.com/BloomGameStudio/PlayerService/database"
 	"github.com/BloomGameStudio/PlayerService/models"
 	"github.com/labstack/echo/v4"
-	"gorm.io/gorm/clause"
 	"strconv"
 	
 
@@ -30,11 +29,6 @@ func DeletePlayer(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid numeric ID.")
 	}
 	queryPlayer.ID = uint(idUint)
-
-    if err := db.Preload(clause.Associations).Where(queryPlayer).First(queryPlayer).Error; err != nil {
-        c.Logger().Error("Failed to retrieve player from the database")
-        return c.JSON(http.StatusInternalServerError, "Failed to retrieve player from the database")
-    }
 
     // Delete the player
     if err := db.Delete(queryPlayer).Error; err != nil {
