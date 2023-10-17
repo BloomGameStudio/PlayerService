faa730d39bce6ac6b8ecb42fe13e0cd55905c39c
Sarahtonein <joemoe1899@gmail.com>
2023-10-17 13:31:22 +1000

    delete player

---
+++ controllers/rest/player/DeletePlayer.go
@@ -0,0 +1,40 @@
+package player
+
+import (
+	"net/http"
+	"github.com/BloomGameStudio/PlayerService/database"
+	"github.com/BloomGameStudio/PlayerService/models"
+	"github.com/labstack/echo/v4"
+	"strconv"
+	
+
+)
+
+func DeletePlayer(c echo.Context) error {
+    // Open the database connection
+    db := database.GetDB()
+
+    idToRemove := c.Param("id")
+
+    if idToRemove == "" {
+        return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid ID.")
+    }
+
+    queryPlayer := &models.Player{}
+
+
+	idUint, err := strconv.ParseUint(idToRemove, 10, 64)
+	if err != nil {
+		c.Logger().Error("Invalid 'id' parameter value. Use a valid numeric ID.")
+		return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid numeric ID.")
+	}
+	queryPlayer.ID = uint(idUint)
+
+    // Delete the player
+    if err := db.Delete(queryPlayer).Error; err != nil {
+        c.Logger().Error("Failed to delete player from the database")
+        return c.JSON(http.StatusInternalServerError, "Failed to delete player from the database")
+    }
+
+    return c.JSON(http.StatusOK, "Player deleted successfully")
+}
