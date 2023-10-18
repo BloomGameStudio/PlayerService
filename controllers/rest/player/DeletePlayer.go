1c40663df07345f29643870be9071e9536d378a0
Sarahtonein <joemoe1899@gmail.com>
2023-10-19 09:20:57 +1000

    Name, UserID Support, changed player route

--- controllers/rest/player/DeletePlayer.go
+++ controllers/rest/player/DeletePlayer.go
@@ -5,6 +5,7 @@ import (
 	"github.com/BloomGameStudio/PlayerService/database"
 	"github.com/BloomGameStudio/PlayerService/models"
 	"github.com/labstack/echo/v4"
+    uuid "github.com/satori/go.uuid"
 	"strconv"
 	
 
@@ -14,26 +15,51 @@ func DeletePlayer(c echo.Context) error {
     // Open the database connection
     db := database.GetDB()
 
-    idToRemove := c.Param("id")
-
-    if idToRemove == "" {
-        return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid ID.")
-    }
+    idParam := c.QueryParam("id")
+    nameParam := c.QueryParam("name")
+    userIDParam := c.QueryParam("userid")
 
     queryPlayer := &models.Player{}
 
+    //check ID
+    if idParam != "" {
+        idUint, err := strconv.ParseUint(idParam, 10, 64)
+        if err != nil {
+            c.Logger().Error("Invalid 'id' parameter value. Use a valid numeric ID.")
+            return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid numeric ID.")
+        }
+        queryPlayer.ID = uint(idUint)
+    }
 
-	idUint, err := strconv.ParseUint(idToRemove, 10, 64)
-	if err != nil {
-		c.Logger().Error("Invalid 'id' parameter value. Use a valid numeric ID.")
-		return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid numeric ID.")
-	}
-	queryPlayer.ID = uint(idUint)
+    // Check Name
+    if nameParam != "" {
+        queryPlayer.Name = nameParam
+    }
 
-    // Delete the player
-    if err := db.Delete(queryPlayer).Error; err != nil {
+    // Check UserID
+    if userIDParam != "" {
+        userID, err := uuid.FromString(userIDParam)
+        if err != nil {
+            c.Logger().Error("Invalid 'userid' parameter value. Use a valid UUID.")
+            return c.JSON(http.StatusBadRequest, "Invalid 'userid' parameter value. Use a valid UUID.")
+        }
+        queryPlayer.UserID = userID
+    }
+    
+
+    // Make sure at least one parameter is provided
+    if idParam == "" && nameParam == "" && userIDParam == "" {
+        return c.JSON(http.StatusBadRequest, "Provide at least one parameter to delete a player")
+    }
+
+    // Delete the player and check if any rows were affected
+    result := db.Where(queryPlayer).Delete(&models.Player{})
+    if result.Error != nil {
         c.Logger().Error("Failed to delete player from the database")
         return c.JSON(http.StatusInternalServerError, "Failed to delete player from the database")
+    }
+    if result.RowsAffected == 0 {
+        return c.JSON(http.StatusNotFound, "No player found to delete")
     }
 
     return c.JSON(http.StatusOK, "Player deleted successfully")
