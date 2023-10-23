ba6eda0cb7e4ba96b2f46862cecd33d816783d1a
Sarahtonein <joemoe1899@gmail.com>
2023-10-23 15:05:35 +1000

    Modified route, deal with log flooding

--- controllers/rest/player/DeletePlayer.go
+++ controllers/rest/player/DeletePlayer.go
@@ -7,60 +7,108 @@ import (
 	"github.com/labstack/echo/v4"
     uuid "github.com/satori/go.uuid"
 	"strconv"
-	
-
+    "gorm.io/gorm"
 )
+
+type Identifier interface {
+    IsMatch(string) bool
+    GetValue() interface{}
+}
+
+type NameIdentifier struct {
+    Value string
+}
+type UUIDIdentifier struct {
+    Value uuid.UUID
+}
+
+type NumericIdentifier struct {
+    Value uint
+}
+
+func (u *UUIDIdentifier) IsMatch(s string) bool {
+    val, err := uuid.FromString(s)
+    if err == nil {
+        u.Value = val
+        return true
+    }
+    return false
+}
+
+func (n *NumericIdentifier) IsMatch(s string) bool {
+    val, err := strconv.ParseUint(s, 10, 64)
+    if err == nil {
+        n.Value = uint(val)
+        return true
+    }
+    return false
+}
+
+func (n *NameIdentifier) IsMatch(s string) bool {
+    _, uidErr := uuid.FromString(s)
+    _, numErr := strconv.ParseUint(s, 10, 64)
+    if uidErr != nil && numErr != nil {
+        n.Value = s
+        return true
+    }
+    return false
+}
+
+
+func (u *UUIDIdentifier) GetValue() interface{} {
+    return u.Value
+}
+
+
+func (n *NumericIdentifier) GetValue() interface{} {
+    return n.Value
+ }
+
+func (n *NameIdentifier) GetValue() interface{} {
+    return n.Value
+}
+
 
 func DeletePlayer(c echo.Context) error {
     // Open the database connection
     db := database.GetDB()
+    id := c.Param("id")
 
-    idParam := c.QueryParam("id")
-    nameParam := c.QueryParam("name")
-    userIDParam := c.QueryParam("userid")
-
-    queryPlayer := &models.Player{}
-
-    //check ID
-    if idParam != "" {
-        idUint, err := strconv.ParseUint(idParam, 10, 64)
-        if err != nil {
-            c.Logger().Error("Invalid 'id' parameter value. Use a valid numeric ID.")
-            return c.JSON(http.StatusBadRequest, "Invalid 'id' parameter value. Use a valid numeric ID.")
-        }
-        queryPlayer.ID = uint(idUint)
-    }
-
-    // Check Name
-    if nameParam != "" {
-        queryPlayer.Name = nameParam
-    }
-
-    // Check UserID
-    if userIDParam != "" {
-        userID, err := uuid.FromString(userIDParam)
-        if err != nil {
-            c.Logger().Error("Invalid 'userid' parameter value. Use a valid UUID.")
-            return c.JSON(http.StatusBadRequest, "Invalid 'userid' parameter value. Use a valid UUID.")
-        }
-        queryPlayer.UserID = userID
-    }
+    identifiers := []Identifier{&UUIDIdentifier{}, &NumericIdentifier{}, &NameIdentifier{}}
     
-
-    // Make sure at least one parameter is provided
-    if idParam == "" && nameParam == "" && userIDParam == "" {
-        return c.JSON(http.StatusBadRequest, "Provide at least one parameter to delete a player")
+    var matchedValue interface{}
+    for _, identifier := range identifiers {
+        if identifier.IsMatch(id) {
+            matchedValue = identifier.GetValue()
+            break
+        }
     }
 
-    // Delete the player and check if any rows were affected
-    result := db.Where(queryPlayer).Delete(&models.Player{})
+    if matchedValue == nil {
+        return c.JSON(http.StatusBadRequest, "Invalid Identifier provided")
+    }
+
+    switch v := matchedValue.(type){
+    case uuid.UUID:
+        result := db.Where("user_id = ?", v).Delete(&models.Player{})
+        return handleDeleteresult(c, result)
+    case uint:
+        result := db.Where("id = ?", v).Delete(&models.Player{})
+        return handleDeleteresult(c, result)
+    case string:
+        result := db.Where("name = ?", v).Delete(&models.Player{})
+        return handleDeleteresult(c, result)
+    default:
+        return c.JSON(http.StatusBadRequest, "Invalid Identifier")
+    }
+}
+
+func handleDeleteresult(c echo.Context, result *gorm.DB) error { 
     if result.Error != nil {
-        c.Logger().Error("Failed to delete player from the database")
         return c.JSON(http.StatusInternalServerError, "Failed to delete player from the database")
     }
     if result.RowsAffected == 0 {
-        return c.JSON(http.StatusNotFound, "No player found to delete")
+        return c.JSON(http.StatusNotFound, "No player found in the database")
     }
-
-    return c.JSON(http.StatusOK, "Player deleted successfully")
+    return c.JSON(http.StatusOK, "Player Deleted successfully.")
 }
