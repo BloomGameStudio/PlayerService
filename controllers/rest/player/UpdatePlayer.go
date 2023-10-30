8ac70979ab1d77c479bcda5bec239bcb4ec10461
Sarahtonein <joemoe1899@gmail.com>
2023-10-30 16:14:30 +1000

    Remove logging lines

--- controllers/rest/player/UpdatePlayer.go
+++ controllers/rest/player/UpdatePlayer.go
@@ -20,27 +20,24 @@ func UpdatePlayer(c echo.Context) error {
 	playerIDStr := c.Param("id")
 
 	if playerIDStr == "" {
-    	return c.JSON(http.StatusBadRequest, "Invalid id parameter value. Use a valid ID")
+		return c.JSON(http.StatusBadRequest, "Invalid id parameter value. Use a valid ID")
 	}
 
 	// Convert playerIDStr to uint
 	playerID, err := strconv.ParseUint(playerIDStr, 10, 64)
 	if err != nil {
-    	c.Logger().Error("Failed to parse player ID")
-    	return c.JSON(http.StatusBadRequest, "Invalid id parameter value. Use a valid ID")
+		return c.JSON(http.StatusBadRequest, "Invalid id parameter value. Use a valid ID")
 	}
-
 
 	// Find the player from ID given
 	queryPlayer := &models.Player{}
 	if err := db.Preload(clause.Associations).Where(playerID).First(queryPlayer).Error; err != nil {
-    	return c.JSON(http.StatusNotFound, "Failed to retrieve player from the database")
+		return c.JSON(http.StatusNotFound, "Failed to retrieve player from the database")
 	}
 
 	// Parse the JSON request body into a separate variable
 	updateData := models.Player{}
 	if err := c.Bind(&updateData); err != nil {
-		c.Logger().Error("Failed to parse update data")
 		return c.JSON(http.StatusBadRequest, "Invalid update data")
 	}
 
@@ -58,11 +55,10 @@ func UpdatePlayer(c echo.Context) error {
 		queryPlayer.States = append(queryPlayer.States, models.State{
 			State: state.State,
 		})
-	} 
+	}
 
 	// Save the updated player
 	if err := db.Save(queryPlayer).Error; err != nil {
-		c.Logger().Error("Failed to update player in the database")
 		return c.JSON(http.StatusInternalServerError, "Failed to update player in the database")
 	}
 
@@ -70,4 +66,3 @@ func UpdatePlayer(c echo.Context) error {
 	return c.JSON(http.StatusOK, queryPlayer)
 
 }
-
