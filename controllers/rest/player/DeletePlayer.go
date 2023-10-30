496a3fa39954dd9d9009220749182fb506f49c6d
Sarahtonein <joemoe1899@gmail.com>
2023-10-31 09:22:34 +1000

    modified interface

--- controllers/rest/player/DeletePlayer.go
+++ controllers/rest/player/DeletePlayer.go
@@ -12,22 +12,20 @@ import (
 )
 
 func DeletePlayer(c echo.Context) error {
-    db := database.GetDB()
+	db := database.GetDB()
 
-    identifier := c.Param("identifier")
-    var query string
-    var arg interface{}
+	identifier := c.Param("identifier")
+	var queryPlayer models.Player
 
-    if _, err := uuid.FromString(identifier); err == nil {
-        query, arg = "user_id = ?", identifier
-    } else if _, err := strconv.Atoi(identifier); err == nil {
-        id, _ := strconv.Atoi(identifier)
-        query, arg = "id = ?", id
-    } else {
-        query, arg = "name = ?", identifier
-    }
-    var queryPlayer models.Player
-	if err := db.Where(query, arg).First(&queryPlayer).Error; err != nil {
+	if uid, err := uuid.FromString(identifier); err == nil {
+		queryPlayer.UserID = uid
+	} else if id, err := strconv.Atoi(identifier); err == nil {
+		queryPlayer.ID = uint(id)
+	} else {
+		queryPlayer.Name = identifier
+	}
+
+	if err := db.Where(&queryPlayer).First(&queryPlayer).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return c.JSON(404, "Player not found")
 		}
@@ -35,13 +33,13 @@ func DeletePlayer(c echo.Context) error {
 		return c.JSON(500, "Database error")
 	}
 
-    result := db.Delete(&models.Player{}, queryPlayer.ID)
-    if result.Error != nil {
-        return c.JSON(http.StatusInternalServerError, "Failed to delete player")
-    }
-    if result.RowsAffected == 0 {
-        return c.JSON(http.StatusNotFound, "No player found to delete")
-    }
+	result := db.Delete(&models.Player{}, queryPlayer.ID)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, "Failed to delete player")
+	}
+	if result.RowsAffected == 0 {
+		return c.JSON(http.StatusNotFound, "No player found to delete")
+	}
 
-    return c.JSON(http.StatusOK, "Player deleted successfully")
+	return c.JSON(http.StatusOK, "Player deleted successfully")
 }
