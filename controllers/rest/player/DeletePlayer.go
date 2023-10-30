f9a32688e9e61ec28e6b0a41ddfc2da83c583ffd
Sarahtonein <joemoe1899@gmail.com>
2023-10-30 13:47:09 +1000

    interface implementation

--- controllers/rest/player/DeletePlayer.go
+++ controllers/rest/player/DeletePlayer.go
@@ -2,57 +2,42 @@ package player
 
 import (
 	"net/http"
+	"strconv"
+
 	"github.com/BloomGameStudio/PlayerService/database"
 	"github.com/BloomGameStudio/PlayerService/models"
 	"github.com/labstack/echo/v4"
-	"strconv"
-    "regexp"
-    "strings"
-    uuid "github.com/satori/go.uuid"
-
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 )
 
 func DeletePlayer(c echo.Context) error {
     db := database.GetDB()
 
     identifier := c.Param("identifier")
-    parts := strings.Split(identifier, ":")
+    var query string
+    var arg interface{}
 
-    queryPlayer := &models.Player{}
-
-    uuidRegex, err := regexp.Compile(`^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$`)
-    if err != nil {
-    // Handle the error if the regex fails to compile
-        return c.JSON(http.StatusInternalServerError, "Internal server error")
+    if _, err := uuid.FromString(identifier); err == nil {
+        query, arg = "user_id = ?", identifier
+    } else if _, err := strconv.Atoi(identifier); err == nil {
+        id, _ := strconv.Atoi(identifier)
+        query, arg = "id = ?", id
+    } else {
+        query, arg = "name = ?", identifier
     }
-    for _, part := range parts {
-        // Use the compiled regex to match the string
-        if uuidRegex.MatchString(part) {
-            uuidValue, err := uuid.FromString(part)
-            if err != nil {
-                return c.JSON(http.StatusBadRequest, "Invalid UUID format")
-            }
-            queryPlayer.UserID = uuidValue
-            continue
-        }
-        
-        if idUint, err := strconv.ParseUint(part, 10, 64); err == nil {
-            queryPlayer.ID = uint(idUint)
-            continue
-        }
-        
-        // If the part is not UUID or ID, then it's assumed to be a name.
-        queryPlayer.Name = part
-    }
+    var queryPlayer models.Player
+	if err := db.Where(query, arg).First(&queryPlayer).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.JSON(404, "Player not found")
+		}
+		// handle other database errors
+		return c.JSON(500, "Database error")
+	}
 
-    if len(parts) < 1 || len(parts) > 3 {
-        return c.JSON(http.StatusBadRequest, "Invalid identifier format")
-    }
-
-    result := db.Where(queryPlayer).Delete(&models.Player{})
+    result := db.Delete(&models.Player{}, queryPlayer.ID)
     if result.Error != nil {
-        c.Logger().Error("Failed to delete player from the database")
-        return c.JSON(http.StatusInternalServerError, "Failed to delete player from the database")
+        return c.JSON(http.StatusInternalServerError, "Failed to delete player")
     }
     if result.RowsAffected == 0 {
         return c.JSON(http.StatusNotFound, "No player found to delete")
