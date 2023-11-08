c2a18867c57be153dca5e22f675d8141684e60c4
Sarahtonein <joemoe1899@gmail.com>
2023-11-08 15:08:51 +1000

    ws godoc

--- controllers/doc.go
+++ controllers/doc.go
@@ -1,2 +1,3 @@
 // Package controllers contains the controller logic for handling both RESTful HTTP and WebSocket requests in the playerService.
+// Please refer to specific documentation related to RESTful HTTP and websocket requests in the appropriate package.
 package controllers
