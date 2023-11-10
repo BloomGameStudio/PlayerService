bb358d50a1f97e56c1834c3c2397279c3ef65bf4
Sarahtonein <joemoe1899@gmail.com>
2023-11-10 11:31:19 +1000

    use errorHandlers

--- controllers/ws/state/stateWriter.go
+++ controllers/ws/state/stateWriter.go
@@ -2,10 +2,10 @@ package state
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"github.com/BloomGameStudio/PlayerService/controllers/ws"
+	"github.com/BloomGameStudio/PlayerService/controllers/ws/errorHandlers"
 	"github.com/BloomGameStudio/PlayerService/database"
 	"github.com/BloomGameStudio/PlayerService/models"
 	"github.com/gorilla/websocket"
@@ -19,7 +19,6 @@ func stateWriter(c echo.Context, socket *websocket.Conn, ch chan error, timeoutC
 	//Use some very old date for the first update
 	lastUpdateAt := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
 	lastPingCheck := time.Now()
-	wsTimeout := time.Second * time.Duration(viper.GetInt("WS_TIMEOUT_SECONDS"))
 
 	for {
 		select {
@@ -39,68 +38,24 @@ func stateWriter(c echo.Context, socket *websocket.Conn, ch chan error, timeoutC
 				err := socket.WriteJSON(states)
 
 				if err != nil {
-					switch {
-					case errors.Is(err, websocket.ErrCloseSent):
-						select {
-						case ch <- nil:
-							c.Logger().Debug("Sent nil to Writer channel")
-							return
-						case <-time.After(wsTimeout):
-							c.Logger().Debug("Timed out sending nil to Writer channel")
-							return
-						}
-					default:
-						c.Logger().Error(err)
-						select {
-						case ch <- err:
-							c.Logger().Debug("Sent error to Writer channel")
-							return
-						case <-time.After(wsTimeout):
-							c.Logger().Debug("Timed out sending error to Writer channel")
-							return
-						}
-					}
+					errorHandlers.HandleWriteError(c, ch, err)
+					return
 				}
-
 			} else if lastPingCheck.Add(time.Second * 1).Before(time.Now()) {
 				c.Logger().Debug("Running Ping Check")
-
 				err := socket.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(time.Second*2))
-
 				if err != nil {
-					switch {
-					case errors.Is(err, websocket.ErrCloseSent):
-						c.Logger().Debug("WEbsocket ErrCloseSent")
-						select {
-						case ch <- nil:
-							c.Logger().Debug("Sent nil to Writer channel")
-							return
-						case <-time.After(wsTimeout):
-							c.Logger().Debug("Timed out sending nil to Writer channel")
-							return
-						}
-					default:
-						c.Logger().Error(err)
-						select {
-						case ch <- err:
-							c.Logger().Debug("Sent error to Writer channel")
-							return
-						case <-time.After(wsTimeout):
-							c.Logger().Debug("Timed out sending error to Writer channel")
-							return
-						}
-					}
+					errorHandlers.HandleReadError(c, ch, err)
+					return
 				}
 			}
 
 			c.Logger().Debug("Finished writing to the WebSocket Sleeping now")
-
 			// Update Interval
 			rate := ws.GetRate(c)
 			time.Sleep(time.Millisecond * time.Duration(rate))
 
 			if viper.GetBool("DEBUG") {
-				// Sleep for x second in DEBUG mode to not get flooded with data
 				time.Sleep(time.Second / 20)
 			}
 		}
