35a3eb56f739d3180d2ad83b72c2c12094f9a0cd
Sarahtonein <joemoe1899@gmail.com>
2023-11-13 13:34:08 +1000

    amend Model timeout

--- controllers/ws/model/Model.go
+++ controllers/ws/model/Model.go
@@ -1,4 +1,4 @@
-package model
+package state
 
 import (
 	"context"
@@ -8,34 +8,30 @@ import (
 )
 
 func Model(c echo.Context) error {
-	// Upgrade the HTTP server connection to the WebSocket protocol
+
 	ws, err := ws.Upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
-		c.Logger().Error("Failed to set websocket upgrade:", err)
 		return err
 	}
 	defer ws.Close()
 
-	// Channels to receive errors (or nil) from reader/writer goroutines
 	writerChan := make(chan error)
 	readerChan := make(chan error)
 
 	timeoutCTX, timeoutCTXCancel := context.WithCancel(context.Background())
 	defer timeoutCTXCancel()
 
-	// Start the reader/writer goroutines
 	go modelWriter(c, ws, writerChan, timeoutCTX)
 	go modelReader(c, ws, readerChan, timeoutCTX)
 
-	select {
-
-	case w := <-writerChan:
-		c.Logger().Debugf("Recieved writerChan error: %v", w)
-		return nil
-
-	case r := <-readerChan:
-		c.Logger().Debugf("Recieved readerChan error: %v", r)
-		return nil
-
+	for {
+		select {
+		case r := <-readerChan:
+			c.Logger().Debugf("Recieved readerChan error: %v", r)
+			return r
+		case w := <-writerChan:
+			c.Logger().Debugf("Recieved writerChan error: %v", w)
+			return w
+		}
 	}
 }
