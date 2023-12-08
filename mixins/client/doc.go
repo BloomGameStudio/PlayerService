0b0b0c515334f1fe44450db7e7e843f5a6dd4fa5
Sarahtonein <joemoe1899@gmail.com>
2023-12-08 17:03:45 +1000

    godoc

---
+++ mixins/client/doc.go
@@ -0,0 +1,16 @@
+// Package client provides mixin functionality for different operations related to websocket connections.
+//
+// Files
+//
+// The package consists of the following files:
+// 
+// - client.go: Contains the ConditionalWriter function that handles...
+//
+// Dependencies:
+//
+// The client package relies on the following packages:
+//
+// - github.com/gorilla/websocket
+//
+// For more information on each function, please refer to their individual documentation.
+package client
